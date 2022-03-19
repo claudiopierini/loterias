202654b797135487f6b0de23e545d529cb9683d9
claudio <claudiopierini@gmail.com>
2022-03-18 23:11:59 -0300

    bla

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,18 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) {
-	    Random gerador = new Random();
-
+        IntStream.range(1, 8).forEachOrdered(i -> {
+            ArrayList<Integer> numbers = (ArrayList<Integer>) new Random().ints(1, 60).limit(6).boxed().collect(Collectors.toList());
+            Collections.sort(numbers);
+            System.out.println("Jogo " + i + " - " + numbers);
+        });
     }
 }
